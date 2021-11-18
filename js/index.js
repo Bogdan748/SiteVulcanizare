8e2bd44c3439c9a6bb751a84d323819946b23560
Bogdan748 <andriesbogdan00@gmail.com>
2021-11-18 19:03:12 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+///////SLIDER
 const slider = function () {
   const slides = document.querySelectorAll(".slide");
   const btnLeft = document.querySelector(".slider__btn--left");
@@ -82,4 +83,92 @@ const slider = function () {
 };
 slider();
 
-// GET AND ARANGE PRETURI
+///////////// MOBILE NAV
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  if (headerEl.classList.contains("nav-open")) {
+    document.body.classList.add("sticky");
+  } else {
+    document.body.classList.remove("sticky");
+  }
+});
+
+////////////////////////////////////////////////
+///Smooth scroling
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    if (e.target.classList.contains("default")) return;
+
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    //Scroll to top
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //Scroll to links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile nav
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+////////////////////sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-250px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
